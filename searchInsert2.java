dcdf830fc8fa9a014b8162a3b318325cdec62ab4
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-13 00:36:56 -0700

    searchInsert leetcode
    This solution is for O(n) with a straight forward approach

---
+++ searchInsert2.java
@@ -0,0 +1,12 @@
+public class searchInsert2 {
+    public int searchInsert(int[] nums, int target) {
+
+        // this solution is for O(n)
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] >= target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
