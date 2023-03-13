3ed9e02e3ec7bbe19eec04059c88d2a4701ab33c
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-13 00:37:28 -0700

    Updating a comment

--- searchInsert2.java
+++ searchInsert2.java
@@ -1,7 +1,7 @@
 public class searchInsert2 {
     public int searchInsert(int[] nums, int target) {
 
-        // this solution is for O(n)
+        // this solution is for O(n) with a straight forward approach.
         for(int i=0; i<nums.length; i++){
             if(nums[i] >= target){
                 return i;
