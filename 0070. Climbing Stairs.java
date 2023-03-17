70d872e19d37e5da0537158501bb0e11465212b6
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-16 21:57:24 -0700

    Updating the style of code

--- 0070. Climbing Stairs.java
+++ 0070. Climbing Stairs.java
@@ -1,11 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        int result = 0; // storing for n = 1
+        if ( n == 1 || n == 2) return n;
+
+        int result = 0;
         int pres = 2;
         int ppres = 1;
-
-        if ( n == 1 ) return 1;
-        if ( n == 2 ) return 2;
 
         for(int i = 2; i < n; i++){
             result = pres + ppres;
