d3973a477962071de18fb271214e89c39447e4e2
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-20 02:45:42 -0700

    uploading solution for 1512. Number of Good Pairs leetcode

---
+++ 1512. Number of Good Pairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int output = 0;
+        for(int i = 0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if((i<j) && (nums[i] == nums[j])) output++;
+            }
+        }
+        return output;
+    }
+}
