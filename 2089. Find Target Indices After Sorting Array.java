5921dcafa7bfb35b581ed3f9fc7e855ed6b8f7c1
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-22 00:09:30 -0700

    Uploading solution for 2089. Find Target Indices After Sorting Array leetcode

---
+++ 2089. Find Target Indices After Sorting Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target) res.add(i);
+        }
+
+        return res;
+    }
+}
