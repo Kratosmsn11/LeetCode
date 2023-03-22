5019b63c625be20b84d8283b48592137add20c95
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-21 23:51:33 -0700

    Uploading solution for 0125. Valid Palindrome leetcode

---
+++ 0125. Valid Palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int i = 0, j = s.length() - 1;
+
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
