176a5730f2c6b224fa114b0f0fb2a26922e19475
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-03-24 14:03:18 -0700

    Uploading solution for 0771. Jewels and Stones leetcode

---
+++ 0771. Jewels and Stones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int res = 0;
+        for(int i=0; i<jewels.length(); i++){
+            for(int j=0; j<stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)) res++;
+            }
+        }
+        return res;
+    }
+}
