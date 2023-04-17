f2d5e5d137653c1e8dbd1765487dd118afdc4435
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-04-16 21:11:24 -0700

    Uploading solution for 1337. The K Weakest Rows in a Matrix leetcode
    This solution has a time complexity of O(m*n)

---
+++ 1337. The K Weakest Rows in a Matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        // using priority queue
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0] : b[1]-a[1]);
+
+        int[] res = new int[k];
+
+        for(int i = 0; i<mat.length; i++){
+            int soldiers = 0;
+            for(int j = 0; j<mat[i].length; j++){
+                if(mat[i][j] == 1) soldiers++;
+                else break;
+            }
+            pq.offer(new int[]{soldiers,i});
+        }
+
+        while(pq.size() > k){
+            pq.poll();
+        }
+
+        while(k>0) res[--k] = pq.poll()[1];
+
+        return res;
+    }
+}
