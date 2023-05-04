d30ccb9eb7bc32e575873ad833b309d9e5b46e07
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-04 16:13:05 -0700

    Uploading solution for 649. Dota2 Senate

---
+++ 0649. Dota2 Senate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+
+        Queue<Integer> rQueue = new LinkedList<>();
+        Queue<Integer> dQueue = new LinkedList<>();
+
+        for(int i = 0; i < n; i++){
+            if(senate.charAt(i) == 'R') rQueue.add(i);
+            else dQueue.add(i);
+        }
+
+        while(!rQueue.isEmpty() && !dQueue.isEmpty()){
+            int rTurn = rQueue.poll();
+            int dTurn = dQueue.poll();
+
+            if(dTurn < rTurn) dQueue.add(dTurn+n);
+            else rQueue.add(rTurn+n);
+        }
+
+        return dQueue.isEmpty() ? "Radiant" : "Dire";
+    }
+}
