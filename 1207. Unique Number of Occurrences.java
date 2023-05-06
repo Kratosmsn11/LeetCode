871525319aa439752f83a078c0f40f15ea9eb404
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-06 14:42:57 -0700

    Uploading solution for 1207. Unique Number of Occurrences

---
+++ 1207. Unique Number of Occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            if(!map.containsKey(arr[i])) map.put(arr[i], 1);
+            else map.put(arr[i], map.get(arr[i])+1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int key : map.keySet()) if(!set.add(map.get(key))) return false;
+
+        return true;
+    }
+}
