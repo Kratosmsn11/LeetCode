02afcf153d9d739cd1f51dd5a8c38ea36dd3979c
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-10 02:01:03 -0700

    Uploading solution 1678. Goal Parser Interpretation.java

---
+++ 1678. Goal Parser Interpretation.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String interpret(String command) {
+        command = command.replace("(al)", "al");
+        command = command.replace("()", "o");
+
+        return command;
+    }
+}
