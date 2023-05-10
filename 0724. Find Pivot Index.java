c2b7d309b68dfcec5e7847d462e4bdcbdd4cf2db
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-10 03:13:11 -0700

    Uploading solution for 724. Find Pivot Index.java

---
+++ 0724. Find Pivot Index.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftSum = 0;
+        for(int n: nums) sum += n;
+        for(int i = 0; i < nums.length; i++){
+            if(leftSum == sum - leftSum - nums[i]) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
