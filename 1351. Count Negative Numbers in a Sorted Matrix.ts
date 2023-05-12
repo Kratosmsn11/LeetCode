bbe7f08c7faf50195fa4caf49398a6916d8f49f6
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-12 15:33:52 -0700

    Uploading solution for 1351. Count Negative Numbers in a Sorted Matrix.ts typescript

---
+++ 1351. Count Negative Numbers in a Sorted Matrix.ts
@@ -0,0 +1,7 @@
+function countNegatives(grid: number[][]): number {
+    let count = 0;
+        for(let i=0; i<grid.length; i++){
+            for(let j = 0; j<grid[i].length; j++) if(grid[i][j] < 0) count++;
+        }
+        return count;
+};
