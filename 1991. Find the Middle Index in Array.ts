49ac5d6976cfddd961e0830ca612c3b5c2c1b250
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-12 16:03:29 -0700

    Uploading solution in typescript for 1991. Find the Middle Index in Array.ts

---
+++ 1991. Find the Middle Index in Array.ts
@@ -0,0 +1,10 @@
+function findMiddleIndex(nums: number[]): number {
+    let sum = 0;
+    let leftSum = 0;
+    for(let i = 0; i < nums.length; i++) sum += nums[i];
+    for(let i = 0; i < nums.length; i++){
+        if(leftSum == (sum-(leftSum+nums[i]))) return i;
+        leftSum += nums[i];
+    }
+    return -1;
+};
