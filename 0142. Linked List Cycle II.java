d6340f1898ca780d9a58239c085be4ab66bc6098
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-15 22:06:41 -0700

    Uploading solution for 0142. Linked List Cycle II.java

---
+++ 0142. Linked List Cycle II.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) break;
+        }
+        if(fast == null || fast.next == null) return null;
+
+        fast = head;
+        while(fast != slow){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return fast;
+    }
+}
