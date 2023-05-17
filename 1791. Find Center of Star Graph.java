2da70324da6f363069dcd064e391283bb41891eb
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-05-17 02:20:30 -0700

    Uploading solution for 1791. Find Center of Star Graph.java

---
+++ 1791. Find Center of Star Graph.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        if(edges[0][0] == edges[1][0]) return edges[0][0];
+        else if(edges[0][0] == edges[1][1]) return edges[0][0];
+        else if(edges[0][1] == edges[1][0]) return edges[0][1];
+        else if(edges[0][1] == edges[1][1]) return edges[0][1];
+
+        return -1;
+    }
+}
