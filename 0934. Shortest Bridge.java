2ff64e9e91b524b9a79c1a5eee324af84349dd16
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-07-01 04:14:27 -0700

    Uploading solution for 0934. Shortest Bridge.java

---
+++ 0934. Shortest Bridge.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int shortestBridge(int[][] grid) {
+        int n = grid.length;
+        int firstX = -1, firstY = -1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    firstX = i;
+                    firstY = j;
+                    break;
+                }
+            }
+        }
+
+        List<int[]> bfsQueue = new ArrayList<>();
+        List<int[]> secondBfsQueue = new ArrayList<>();
+        bfsQueue.add(new int[]{firstX, firstY});
+        secondBfsQueue.add(new int[]{firstX, firstY});
+        grid[firstX][firstY] = 2;
+
+        while (!bfsQueue.isEmpty()) {
+            List<int[]> newBfs = new ArrayList<>();
+            for (int[] cell : bfsQueue) {
+                int x = cell[0];
+                int y = cell[1];
+                for (int[] next : new int[][]{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}) {
+                    int curX = next[0];
+                    int curY = next[1];
+                    if (curX >= 0 && curX < n && curY >= 0 && curY < n && grid[curX][curY] == 1) {
+                        newBfs.add(new int[]{curX, curY});
+                        secondBfsQueue.add(new int[]{curX, curY});
+                        grid[curX][curY] = 2;
+                    }
+                }
+            }
+            bfsQueue = newBfs;
+        }
+
+        int distance = 0;
+        while (!secondBfsQueue.isEmpty()) {
+            List<int[]> newBfs = new ArrayList<>();
+            for (int[] cell : secondBfsQueue) {
+                int x = cell[0];
+                int y = cell[1];
+                for (int[] next : new int[][]{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}) {
+                    int curX = next[0];
+                    int curY = next[1];
+                    if (curX >= 0 && curX < n && curY >= 0 && curY < n) {
+                        if (grid[curX][curY] == 1) {
+                            return distance;
+                        } else if (grid[curX][curY] == 0) {
+                            newBfs.add(new int[]{curX, curY});
+                            grid[curX][curY] = -1;
+                        }
+                    }
+                }
+            }
+
+            secondBfsQueue = newBfs;
+            distance++;
+        }
+        return distance;
+    }
+}
