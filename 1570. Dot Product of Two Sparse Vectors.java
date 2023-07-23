be276896e7219f667d76b3e7eafd580ff69c07c6
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-07-22 20:47:52 -0700

    Uploading solution for 1570. Dot Product of Two Sparse Vectors.java

---
+++ 1570. Dot Product of Two Sparse Vectors.java
@@ -0,0 +1,23 @@
+class SparseVector {
+    
+    private int[] array;
+    SparseVector(int[] nums) {
+        array = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+
+        for(int i = 0; i < array.length; ++i){
+            result += array[i] * vec.array[i];
+        }
+
+        return result;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
