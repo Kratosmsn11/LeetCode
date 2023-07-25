50ce649b9fd40ee3c1702f77599b729fde26bc9a
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-07-24 23:22:57 -0700

    Uploading solution for 172. Factorial Trailing Zeroes.java

---
+++ 0172. Factorial Trailing Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        for(int i = 5; i <= n; i += 5){
+            int curr = i;
+            while(curr % 5 == 0){
+                res++;
+                curr /= 5;
+            }
+        }
+        return res;
+    }
+}
