4292b3746587666f5f705d804b1f3e4ca66ab045
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-07-24 17:47:37 -0700

    Added some explanation in the code, with understandable variable names

--- 0852. Peak Index in a Mountain Array.java
+++ 0852. Peak Index in a Mountain Array.java
@@ -1,11 +1,27 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i = 0, j = arr.length-1;
-        while(i<j){
-            int mid = i + (j-i)/2;
-            if(arr[mid]>arr[mid+1]) j = mid;
-            else i = mid+1;
+        // using two variables start & end.
+        int start = 0, end = arr.length - 1;
+
+        // approach : binary search
+        while(start < end){
+            // calculating mid using start & end.
+            int mid = start + (end - start)/2;
+
+            // IF an element at mid is greater than an element at mid + 1,
+            // then we know that the mid or any index smaller than mid is
+            // the peak. 
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            // Else the mid we move to check the later half, by setting the
+            // start to mid + 1.
+            else{
+                start = mid + 1;
+            }
         }
-        return i;
+        
+        // we can return either start or end as both are equal.
+        return start;
     }
 }
