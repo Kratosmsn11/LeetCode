6f952619706743e4db58abb4dcbc21c9a3d26bf5
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-09-12 02:50:22 -0700

    Basic updates to the code

--- 1647. Minimum Deletions to Make Character Frequencies Unique.java
+++ 1647. Minimum Deletions to Make Character Frequencies Unique.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minDeletions(String s) {
         int[] freq = new int[26];
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.length(); ++i){
             freq[s.charAt(i) - 'a']++;
         }
 
@@ -9,8 +9,8 @@ class Solution {
         HashSet<Integer> seen = new HashSet<>();
         for(int num: freq){
             while(num > 0 && seen.contains(num)){
-                num--;
-                result++;
+                --num;
+                ++result;
             }
             seen.add(num);
         }
