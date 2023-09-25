0f68971269fb659d8a2cac0c890a861a6c0c2596
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-09-25 01:48:50 -0700

    Uploading solution for 2626. Array Reduce Transformation.js

---
+++ 2626. Array Reduce Transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let result = init;
+    for(const element of nums){
+        result = fn(result, element);
+    }
+
+    return result;
+};
